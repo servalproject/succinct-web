0f0d7e9c1c2ea2f8fd743e9042671da3c921e2f6
Kieran Clancy <kieran.clancy@flinders.edu.au>
2017-10-03 17:55:43 +1030

    Setup: basic apache configuration

--- setup/install.sh
+++ setup/install.sh
@@ -2,4 +2,25 @@
 
 ((EUID==0)) || { echo "Run this script as root e.g. sudo" "$@"; exit 2; }
 
-apt-get install apache2
+set -x
+
+cd $(dirname "$0")
+
+apt-get install -y apache2
+
+a2enmod proxy_wstunnel
+service apache2 restart
+
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
+
+adduser --system --home /srv/succinct --group succinct
+
+cp -a ../www /srv/succinct
+chown -R succinct:succinct /srv/succinct/www
+
+cp apache/succinct.conf /etc/apache2/sites-available
+
+a2dissite 000-default
+a2ensite succinct
+service apache2 reload
